470c031311ba112051921494fd05b0319d3ae920
ZakariaBenali <benalizak97@gmail.com>
2023-03-05 11:57:52 +0100

    chore(example): format code using prettier

--- examples/vite-react/src/components/CustomTabs/CustomTabsComponent.tsx
+++ examples/vite-react/src/components/CustomTabs/CustomTabsComponent.tsx
@@ -1,13 +1,12 @@
-import { FC, HTMLAttributes } from 'react'
-import { withTabs } from 'react-with-tabs'
+import { FC, HTMLAttributes } from 'react';
+import { withTabs } from 'react-with-tabs';
 
-const Component: FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...rest}) => {
-  return (
-    <div className="custom-tabs" {...rest}>
-        {children}
-    </div>
-  )
-}
-
+const Component: FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...rest }) => {
+	return (
+		<div className="custom-tabs" {...rest}>
+			{children}
+		</div>
+	);
+};
 
 export const CustomTabsComponent = withTabs(Component);
--- examples/vite-react/src/components/DefaultTabs.tsx
+++ examples/vite-react/src/components/DefaultTabs.tsx
@@ -1,16 +1,16 @@
-import { Tab, TabList, TabPanel, Tabs } from "react-with-tabs";
+import { Tab, TabList, TabPanel, Tabs } from 'react-with-tabs';
 
 const DefaultTabs = () => {
-  return (
-      <Tabs>
-        <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-        </TabList>
-        <TabPanel>Tab content 1</TabPanel>
-        <TabPanel>Tab content 2</TabPanel>
-      </Tabs>
-  );
+	return (
+		<Tabs>
+			<TabList>
+				<Tab>Tab 1</Tab>
+				<Tab>Tab 2</Tab>
+			</TabList>
+			<TabPanel>Tab content 1</TabPanel>
+			<TabPanel>Tab content 2</TabPanel>
+		</Tabs>
+	);
 };
 
-export default DefaultTabs;
+export { DefaultTabs };
