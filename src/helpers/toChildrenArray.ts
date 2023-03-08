f699cbcaedc9c5b35af15788965869cc14f79bc5
ZakariaBenali <benalizak97@gmail.com>
2023-03-08 11:17:48 +0100

    fix: add support for more children use cases

--- src/helpers/toChildrenArray.ts
+++ src/helpers/toChildrenArray.ts
@@ -1,9 +1,22 @@
-import React from 'react';
+import { Children, ReactElement, ReactNode, isValidElement } from 'react';
 
-export function toChildrenArray(children: React.ReactNode) {
-	if (Array.isArray(children)) {
-		return children;
-	} else {
-		return React.Children.toArray(children);
+export function toChildrenArray(children: ReactNode) {
+	const childrenArray: ReactElement[] = Array.isArray(children) ? children : Children.toArray(children);
+	const result: ReactElement[] = [];
+
+	for (let i = 0; i < childrenArray.length; i++) {
+		const curr = childrenArray[i];
+		if (Array.isArray(curr)) {
+			for (let j = 0; j < curr.length; j++) {
+				const child = curr[j];
+				if (isValidElement(child)) {
+					result.push(child);
+				}
+			}
+		} else if (isValidElement(curr)) {
+			result.push(curr);
+		}
 	}
+
+	return result;
 }
